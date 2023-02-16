9c64bb5353bea6b9ed60f427dea559c163cd137f
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-02-16 15:30:51 +0000

    add Type resolver and ensure type safety

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,17 +1,22 @@
 import { Entity, Property, PrimaryKey } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
-
+    @Field(() => Int)
     @PrimaryKey()
     id!: number;
 
+    @Field(() => String)
     @Property({type: "date", default: "NOW()"})
     createdAt?: Date = new Date();
 
+    @Field(() => String)
     @Property({ type: "date", onUpdate: () => new Date() })
     updatedAt?: Date = new Date();
 
+    @Field()
     @Property({type: "text"})
     title!: string;
     
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -18,9 +19,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false,
         }),
+        context: (() => ({ em: orm.em }))
     });
 
     await apolloServer.start()
