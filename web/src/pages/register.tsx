e06867eaa3fa376570b4b6391c452089a5a07c01
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-02-22 12:31:17 +0000

    set up register function with a graphql query. fixing CORS error.

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -10,16 +10,34 @@ import {
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password:String!) {
+  register(options: { username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
+          return register(values)
         }}
       >
         {({ isSubmitting }) => (
