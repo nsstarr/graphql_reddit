9e1742d4de15cd6ac305c25e274151116500570f
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-06 15:23:11 +0000

    navbar with next js client side rendered links to login and password

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Flex bg="tomato" p="4">
+  
+      <Box ml={"auto"}>
+        <NextLink href="/login">
+          <Button mr={4} color="white">
+            login
+          </Button>
+        </NextLink>
+        <NextLink href="/register">
+          <Button color="white">register</Button>
+        </NextLink>
+      </Box>
+    </Flex>
+  );
+};
